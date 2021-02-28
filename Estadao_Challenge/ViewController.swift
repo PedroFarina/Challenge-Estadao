9efe19497fccd5a6327ef34bb3afe77c03be2fa2
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2021-02-27 23:39:59 -0300

    Initial Commit

---
+++ Estadao_Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Estadao_Challenge
+//
+//  Created by Pedro Giuliano Farina on 27/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
