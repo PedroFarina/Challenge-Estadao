240455e2f078e8196912f30d1a1ebd9f0a326e69
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2021-03-02 23:42:33 -0300

    Fixing wrong hierarchy code

--- Estadao_Challenge/Model/NSAttributedString Extension.swift
+++ Estadao_Challenge/Model/NSAttributedString Extension.swift
@@ -26,16 +26,3 @@ internal extension NSAttributedString {
         return mutableString
     }
 }
-
-extension UIImage {
-    func processImage(toWidth width: CGFloat) -> UIImage? {
-        let newSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
-
-        UIGraphicsBeginImageContextWithOptions(newSize, false, scale)
-        draw(in: CGRect(origin: .zero, size: newSize))
-        defer {
-            UIGraphicsEndImageContext()
-        }
-        return UIGraphicsGetImageFromCurrentImageContext()
-    }
-}
---
+++ Estadao_Challenge/Model/UIImageExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UIImageExtension.swift
+//  Estadao_Challenge
+//
+//  Created by Pedro Giuliano Farina on 02/03/21.
+//
+
+import UIKit
+
+extension UIImage {
+    func processImage(toWidth width: CGFloat) -> UIImage? {
+        let newSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, scale)
+        draw(in: CGRect(origin: .zero, size: newSize))
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
